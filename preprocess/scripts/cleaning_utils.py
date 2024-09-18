7fd1c6647fc10b9790db5ab9f59e068825ee21e9
alexandre-assad <alexandre.assad@laplateforme.io>
2024-09-18 16:05:38 +0200

    :wip: data cleaning drop

---
+++ preprocess/scripts/cleaning_utils.py
@@ -0,0 +1,27 @@
+from numpy import percentile
+from pandas import DataFrame
+
+def drop_outliers(
+    dataframe: DataFrame, columns: list[str], percent: int = 75
+) -> DataFrame:
+    """
+    With Tukey's method to find outliers, we change quartile by input percentile & drop outliers
+    """
+    for column in columns:
+        if dataframe[column].dtype not in ["int64", "float64"]:
+            continue
+        first_quartile = percentile(dataframe[column], 100 - percent)
+        third_quartile = percentile(dataframe[column], percent)
+        step = 1.5 * (third_quartile - first_quartile)
+
+        dataframe.drop(
+            dataframe.loc[
+                ~(
+                    (dataframe[column] >= first_quartile - step)
+                    & (dataframe[column] <= third_quartile + step)
+                ),
+                column,
+            ].index,
+            inplace=True,
+        )
+    return dataframe
