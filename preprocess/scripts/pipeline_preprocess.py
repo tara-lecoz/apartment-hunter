ebbc07d9776ef34041ad667dc6e04676b6832c57
alexandre-assad <alexandre.assad@laplateforme.io>
2024-09-26 16:39:46 +0200

    :feat: feature selection

--- preprocess/scripts/pipeline_preprocess.py
+++ preprocess/scripts/pipeline_preprocess.py
@@ -1,18 +1,24 @@
 from pandas import DataFrame, concat
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
+
 
 
 
 def pipeline_model(dataframe) -> DataFrame:
     categorical_columns = [column for column in dataframe.columns if dataframe[column].dtype == 'object']
     numerical_columns = [column for column in dataframe.columns if dataframe[column].dtype in ['int64', 'float64']]
+    all_columns = dataframe.columns
 
-    encoder=OneHotEncoder(sparse=False)
+    encoder=OneHotEncoder(sparse_output=False)
     encoder.set_output(transform='pandas')
-    dataframe[categorical_columns] = e
-    scaler = StandardScaler()
-    dataframe = scaler.fit_transform(dataframe)
+    dataframe_categorical = encoder.fit_transform(dataframe[categorical_columns])
 
+    dataframe.drop(columns=categorical_columns, inplace=True)
+    dataframe = concat([dataframe, dataframe_categorical], axis=1)
+    
+    scaler = StandardScaler()
+    for column in numerical_columns:
+        dataframe[[column]] = scaler.fit_transform(dataframe[[column]])
+    
     return dataframe
